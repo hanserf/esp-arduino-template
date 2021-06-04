52c365a86ba9900a6e3e5a9d0f5490fcd7c72a7a
Hans Erik Fjeld <hans.erik.fjeld@embida.no>
2021-06-04 10:40:12 +0200

    Build on windows master/master

--- idf-project/patches.sh
+++ idf-project/patches.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sed -i "s:set(includedirs:set(includedirs\n  tools/sdk/esp32/include/esp_rainmaker/include\n  tools/sdk/esp32/include/qrcode/include:g" components/arduino/CMakeLists.txt
+sed -i "s:#include <sntp.h>:#include <esp_sntp.h>:g
